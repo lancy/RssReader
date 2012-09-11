dfec1b29bdffb96f64c2e8da53972ccdfae12222
lancy <lancy1014@gmail.com>
2012-09-11 17:28:54 +0800

    fixed

--- RssReader/DetailViewController.m
+++ RssReader/DetailViewController.m
@@ -28,8 +28,6 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
-    NSLog(@"%@", self.item);
-    NSLog(@"content = %@", [self.item summary]);
     UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - 44)];
     [self.view addSubview:webView];
     [webView loadHTMLString:self.item.summary baseURL:[NSURL URLWithString:self.item.link]];
